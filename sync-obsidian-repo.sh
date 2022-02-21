c31a8ffe1c53c1d6a81290f3da2f4838c5209309
Roberto Gongora <robertogongora@hey.com>
2022-02-21 10:28:39 -0600

    Initial Commit.
    Added script with repo initialisation, sync files from a static folder and push them to GitHub.
    
    Possible refactors may include converting the REPO and VAULT dirs to arguments, or figuring everything out from an envfile.

---
+++ sync-obsidian-repo.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+REPO_DIR=~/.obsidian-repo
+VAULT_DIR=~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Silverwing/
+DATE_TIME=$(date "+%Y-%m-%d +%H-%M-%S")
+
+# Create folder if it doesnt exist
+if [ ! -d "$REPO_DIR" ]; then
+    mkdir -p $REPO_DIR
+    git init --bare $REPO_DIR
+    git --git-dir="$REPO_DIR" --work-tree="$VAULT_DIR" remote add origin https://github.com/RobertoGongora/obsidian-repo.git
+fi
+
+# Add the vault folder to the repo
+git --git-dir="$REPO_DIR" --work-tree="$VAULT_DIR" add .
+git --git-dir="$REPO_DIR" --work-tree="$VAULT_DIR" commit -m "Backed up vault on $DATE_TIME"
+git --git-dir="$REPO_DIR" --work-tree="$VAULT_DIR" push origin main
