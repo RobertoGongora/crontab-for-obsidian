8a58fe35990009c9236f236446ff9aa62fcd2455
Roberto Gongora <robertogongora@hey.com>
2022-03-24 18:40:10 -0600

    Fixed initial repo creation

--- sync-obsidian-repo.sh
+++ sync-obsidian-repo.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 REPO_DIR=~/.obsidian-repo
+REPO_URL=https://github.com/RobertoGongora/obsidian-repo.git
 VAULT_DIR=~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Silverwing/
 DATE_TIME=$(date "+%Y-%m-%d %H-%M-%S")
 
 # Create folder if it doesnt exist
 if [ ! -d "$REPO_DIR" ]; then
-    git pull https://github.com/RobertoGongora/obsidian-repo.git $REPO_DIR
+    git clone --bare --depth 1 "$REPO_URL" "$REPO_DIR"
 fi
 
 # Add the vault folder to the repo
